fa541fe229548b9fdb2c40e904b22388338dbd60
Foxy22 <85758375+Foxy221345@users.noreply.github.com>
2022-11-27 19:03:00 -0600

    Add files via upload

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 model_status="";
 input_value="";
 canvas="";
+objects=[];
+percent="";
 function setup()
 {
     canvas=createCanvas(300,300)
@@ -28,4 +30,47 @@ function modeLoaded()
 function draw()
 {
     image(video, 0,0,300, 300);
+
+    if(model_status!="")
+    {
+        for(i=0;i<objects.length;i++)
+        {
+            percent=floor(objects[i].confidence*100);
+      fill("purple");
+      text(objects[i].label+" "+percent+"%",objects[i].x, objects[i].y );
+      noFill();
+      stroke("purple");
+      rect(objects[i].x, objects[i].y, objects[i].width, objects[i].height);
+        }
+
+        if(percent==input_value)
+        {
+            video.stop();
+            object_detector=ml5.objectDetector("cocossd", gotResult);
+            document.getElementById("status").innerHTML="status =" +input_value+" found!";
+            utterThis=new SpeechSynthesisUtterance( input_value +" found !!");
+            synth.speak(utterThis);//
+
+        }
+        else
+        {
+         document.getElementById("status").innerHTML="status = "+ input_value+" not found"
+        }
+    }
 }
+
+function gotResult(error, results)
+{
+ if(error)
+ {
+    console.error();
+ }
+ else
+ {
+    console.log(results);
+    objects=results;
+
+ }
+}
+
+   
